31267513405e1353170c942d2a25ae4b0cb8f45a
DhanyGanz <105541316+DhanyGanz@users.noreply.github.com>
2022-05-15 16:33:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
-   * Follow https://github.com/DikaArdnt
+   * Create By DhanyGanz.
+   * Contact Me on wa.me/6285733374626
+   * Follow https://github.com/DhanyGanz
 */
 
 const fs = require('fs')
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
+global.owner = ['6285733374626','6281382420312','6288292024190']
 global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.packname = 'Akihiko - Daichi'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
